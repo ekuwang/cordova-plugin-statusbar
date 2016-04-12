9ed83bea3d98700aec793ed5d185384ea736f126
Anagani Sai Kiran <saikirananagani7@gmail.com>
2016-04-12 12:07:16 +0530

    added support to 5.1 to above

--- src/android/StatusbarTransparent.java
+++ src/android/StatusbarTransparent.java
@@ -13,6 +13,30 @@ public class StatusbarTransparent extends CordovaPlugin {
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
+		// Create/Set toolbar as actionbar
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		
+		// Check if the version of Android is Lollipop or higher
+		if (Build.VERSION.SDK_INT >= 21) {
+		
+		    // Set the status bar to dark-semi-transparentish
+		    getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+		            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+		
+		    // Set paddingTop of toolbar to height of status bar.
+		    // Fixes statusbar covers toolbar issue
+		    toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+		}
+		// A method to find height of the status bar
+		public int getStatusBarHeight() {
+		    int result = 0;
+		    int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+		    if (resourceId > 0) {
+		        result = getResources().getDimensionPixelSize(resourceId);
+		    }
+		    return result;
+		}
 		// grab the correct methods
 		if(action.equalsIgnoreCase("enable")) {
 			if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
